23817ba418ed693f7e552c93055ea3897e85d290
brrusselburg <25828824+brrusselburg@users.noreply.github.com>
2026-08-29 16:44:28 -0500

    Adds .claude and updates brewfile

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -5,30 +5,28 @@ alias py="python3"
 # Navigation
 alias dev="cd /Users/benjirusselburg/dev"
 alias home="cd /Users/benjirusselburg"
-alias cls="clear" # Good 'ol Clear Screen command
 
 # Git
 alias uhhh="git for-each-ref --sort='authordate:iso8601' --format=' %(color:green)%(authordate:relative)%09%(color:white)%(refname:short) / %(contents:subject)' refs/heads"
 
 # Work
-alias dcore="cd ~/dev/discourse/discourse"
-alias dplugins="cd ~/dev/discourse/plugins"
-alias dthemes="cd ~/dev/discourse/themes"
-alias dserver="cd ~/dev/discourse/discourse; rm -rf tmp; rails s"
-alias dember="cd ~/dev/discourse/discourse; bin/ember-cli"
-alias dstart="cd ~/dev/discourse/discourse; bin/ember-cli -u"
-alias dinstall="bundle;pnpm install;rake db:migrate"
-alias dmigratetest="LOAD_PLUGINS=1 RAILS_ENV=test bin/rake db:migrate"
-alias drefresh="cd ~/dev/discourse/discourse;gl;dInstall;dMigrateTest;dStart"
-alias dreload="cd ~/dev/discourse/discourse;bin/ember-cli -u"
-alias remakedb-test="LOAD_PLUGINS=1 bin/rails db:drop db:create db:migrate RAILS_ENV=test"
-alias claude="AWS_ACCESS_KEY_ID=\$(op item get 'benji - claude on bedrock' --account=discourse --fields='access key id' --reveal) AWS_SECRET_ACCESS_KEY=\$(op item get 'benji - claude on bedrock' --account=discourse --fields='secret access key' --reveal) AWS_REGION=us-east-1 CLAUDE_CODE_USE_BEDROCK=1 claude"
-alias agc="cd ~/dev/discourse/discourse/plugins/discourse-global-communities"
+# alias dcore="cd ~/dev/discourse/discourse"
+# alias dplugins="cd ~/dev/discourse/plugins"
+# alias dthemes="cd ~/dev/discourse/themes"
+# alias dserver="cd ~/dev/discourse/discourse; rm -rf tmp; rails s"
+# alias dember="cd ~/dev/discourse/discourse; bin/ember-cli"
+# alias dstart="cd ~/dev/discourse/discourse; bin/ember-cli -u"
+# alias dinstall="bundle;pnpm install;rake db:migrate"
+# alias dmigratetest="LOAD_PLUGINS=1 RAILS_ENV=test bin/rake db:migrate"
+# alias drefresh="cd ~/dev/discourse/discourse;gl;dInstall;dMigrateTest;dStart"
+# alias dreload="cd ~/dev/discourse/discourse;bin/ember-cli -u"
+# alias remakedb-test="LOAD_PLUGINS=1 bin/rails db:drop db:create db:migrate RAILS_ENV=test"
+# alias agc="cd ~/dev/discourse/discourse/plugins/discourse-global-communities"
 
-alias gm="dcore
-git checkout main
-git pull
-bundle
-pnpm i
-dmigratetest
-bin/rails db:migrate"
+# alias gm="dcore
+# git checkout main
+# git pull
+# bundle
+# pnpm i
+# dmigratetest
+# bin/rails db:migrate"
